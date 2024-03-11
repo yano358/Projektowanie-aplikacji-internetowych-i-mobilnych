c84e8793926a8ce2cdec3327d43bd86a5058d7e9
yano358 <jan.gumulakdev@gmail.com>
2024-03-11 17:49:44 +0100

    added prettier and eslint settings

--- dwarf-hunt/src/app/page.tsx
+++ dwarf-hunt/src/app/page.tsx
@@ -1,11 +1,11 @@
-'use client';
-import { Button } from '@mui/material';
+"use client";
+import { Button } from "@mui/material";
 
 export default function Home() {
   return (
     <div className="flex justify-center flex-start">
-      <h1 >Dwarf Hunt</h1>
-      <Button href='secondpg'> asasdasd </Button>
+      <h1>Dwarf Hunt</h1>
+      <Button href="secondpg"> asasdasd </Button>
     </div>
   );
 }
