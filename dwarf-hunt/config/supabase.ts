2379b1536c1ecb5d3071ddfc2f3b241cd27d66dd
Michal <michalzajdel2002@gmail.com>
2024-03-20 19:39:54 +0100

    deleted useless imports

--- dwarf-hunt/config/supabase.ts
+++ dwarf-hunt/config/supabase.ts
@@ -1,8 +1,5 @@
 "use server";
 import { createClient } from "@supabase/supabase-js";
-import { cookies } from "next/headers";
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
-import { NextResponse, type NextRequest } from "next/server";
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
