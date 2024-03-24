c3e11bc2064ab07f5502e0ec0235a843ee01a2d5
Xeviz <89038819+Xeviz@users.noreply.github.com>
2024-03-24 19:28:27 +0100

    Feature/achievements (#9)
    * your achievements page
    
    * improved your achievements page
    
    * loading logged user's achievements

---
+++ dwarf-hunt/components/AchievedBox.tsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import { Card, Toolbar, Button } from "@mui/material";
+import { styled } from "@mui/system";
+import Link from "next/link";
+
+interface AchievementData {
+  name: string;
+  description: string;
+  aqusition_date: string;
+  is_achieved: boolean;
+}
+
+const BlackCard = styled(Card)(({ isAchieved }: { isAchieved: boolean }) => ({
+  backgroundColor: isAchieved ? "#4a4343" : "black",
+  color: "white",
+  width: 336,
+  height: 200,
+  position: "relative",
+  justifyContent: "between",
+  alignItems: "center",
+  border: isAchieved ? "4px solid black" : "4px solid #4a4343",
+  borderRadius: 10,
+  margin: 10,
+}));
+
+const NameTile = styled(Card)({
+  backgroundColor: "inherit",
+  color: "white",
+  height: "20%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  margin: 0,
+  borderBottomLeftRadius: 0,
+  borderBottomRightRadius: 0,
+  borderBottom: "2px solid red",
+});
+
+const DescriptionTile = styled(Card)({
+  fontSize: "22px",
+  backgroundColor: "inherit",
+  color: "white",
+  height: "60%",
+  display: "flex",
+  margin: 0,
+  padding: "5px 10px",
+  borderBottom: "2px solid red",
+  textAlign: "justify",
+  borderRadius: 0,
+});
+
+const DateTile = styled(Card)({
+  fontSize: "10px",
+  backgroundColor: "inherit",
+  color: "white",
+  height: "20%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  margin: 0,
+  borderTopLeftRadius: 0,
+  borderTopRightRadius: 0,
+  padding: "5px 10px",
+});
+
+const AchievedBox: React.FC<AchievementData> = ({
+  name,
+  description,
+  aqusition_date,
+  is_achieved,
+}) => {
+  const formattedDate = aqusition_date.slice(0, 10);
+
+  return (
+    <BlackCard isAchieved={is_achieved}>
+      <NameTile>{name}</NameTile>
+      <DescriptionTile>{description}</DescriptionTile>
+      <DateTile>{"obtained at: " + formattedDate}</DateTile>
+    </BlackCard>
+  );
+};
+
+export default AchievedBox;
---
+++ dwarf-hunt/src/app/achievements/page.tsx
@@ -0,0 +1,113 @@
+"use client";
+import { Box, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { supabase } from "../../../config/supabase";
+import { checkSesh } from "../actions/index";
+import { signUpSupabaseServerClient } from "../../../config/signUpClient";
+import NavBar from "../../../components/NavBar";
+import AchievedBox from "../../../components/AchievedBox";
+import TitleStrip from "../../../components/TitleStrip";
+
+const YourAchievementsPage = () => {
+  const [achievedAchievements, setAchieved] = useState<any[]>([]);
+  const [unachievedAchievements, setUnachieved] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userAchievements = await fetchUserAchievements();
+        const achievedIds = userAchievements.map(
+          (achievement: any) => achievement.achievement_id
+        );
+        setAchieved(userAchievements);
+        const notAchieved = await fetchNotAchieved(achievedIds);
+        setUnachieved(notAchieved);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const fetchUserAchievements = async () => {
+    try {
+      const currentUserId = (await checkSesh()).user?.id;
+      const { data, error } = await supabase
+        .from("user_achievements")
+        .select(
+          "achievement_id, created_at, accounts(user_id , username), achievements(id, name, description)"
+        )
+        .eq("user_id", currentUserId);
+      if (error) {
+        throw error;
+      }
+      return data;
+    } catch (error) {
+      console.error("Error fetching user achievements:", error);
+      return [];
+    }
+  };
+
+  const fetchNotAchieved = async (achievedIds: string[]) => {
+    try {
+      const idsString = `(${achievedIds.join(",")})`;
+      const { data, error } = await supabase
+        .from("achievements")
+        .select("id, name, description")
+        .not("id", "in", idsString);
+      if (error) {
+        throw error;
+      }
+      return data;
+    } catch (error) {
+      console.error("Error fetching not achieved achievements:", error);
+      return [];
+    }
+  };
+
+  if (isLoading) {
+    return <Typography>Loading Achievements...</Typography>;
+  }
+
+  return (
+    <Box>
+      <NavBar />
+      <TitleStrip textData={"Your Achievements:"}></TitleStrip>
+      <Box display="flex" flexWrap="wrap">
+        {achievedAchievements.map((achievement, index) => (
+          <Box key={index} marginRight={1} marginBottom={1}>
+            <Typography variant="body1">
+              <AchievedBox
+                name={achievement.achievements.name}
+                description={achievement.achievements.description}
+                aqusition_date={achievement.created_at}
+                is_achieved={true}
+              ></AchievedBox>
+            </Typography>
+          </Box>
+        ))}
+      </Box>
+
+      <TitleStrip textData={"Not Achieved:"}></TitleStrip>
+      <Box display="flex" flexWrap="wrap">
+        {unachievedAchievements.map((achievement, index) => (
+          <Box key={index} marginRight={1} marginBottom={1}>
+            <Typography variant="body1">
+              <AchievedBox
+                name={achievement.name}
+                description={achievement.description}
+                aqusition_date="TBD"
+                is_achieved={false}
+              ></AchievedBox>
+            </Typography>
+          </Box>
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default YourAchievementsPage;
